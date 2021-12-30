e68bb7e1851e9ddbe14aec6d53b8bc9374d190d4
sou <opepelab@gmail.com>
2021-12-31 01:32:50 +0900

    firstco mmit

--- components/Text.tsx
+++ components/Text.tsx
@@ -3,8 +3,8 @@ import ActiveLink from "./Sys/ActiveLink";
 const Text: React.FC = () => {
   return (
     <div className="migiue inblo">
-      <ActiveLink href="/" activeClassName="headerState">
-        <a>こやどの会 ミーティング</a>
+      <ActiveLink href="/" activeClassName="">
+        <a>こやどの会ミーティングプロジェクト</a>
       </ActiveLink>
     </div>
   );
