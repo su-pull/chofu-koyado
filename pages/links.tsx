43c0ad0219e51a49cb08e6c87a6503dd139541e2
sou <opepelab@gmail.com>
2022-01-06 02:46:03 +0900

    first commit

--- pages/links.tsx
+++ pages/links.tsx
@@ -14,28 +14,37 @@ const Links: React.FC = () => {
         <div className="triangle-bottom" />
         <dl>
           <dd>
-            <a className="redLinks scaleLinks" href="https://www.ccsw.or.jp" target="_blank">
+            <a href="https://soradai.kenseikai-group.or.jp/" className="blue scaleLinks" target="_blank">
+              空と大地と
+            </a>
+          </dd>
+          <dd>
+            <a href="https://www.tokyo-satsuki.jp/" className="satsuki scaleLinks" target="_blank">
+              さつきホスピタル病院
+            </a>
+          </dd>
+          <dd>
+            <a className="tyairo scaleLinks" href="https://www.ccsw.or.jp" target="_blank">
               社会福祉協議会
             </a>
           </dd>
           <dd>
-            <a className="redLinks scaleLinks" href="https://ikizura-wh.jimdofree.com" target="_blank">
+            <a className="green scaleLinks" href="https://ikizura-wh.jimdofree.com" target="_blank">
               生きづらわーほりプロジェクト
             </a>
           </dd>
           <dd>
-            <a className="redLinks scaleLinks" href="/" target="_blank">
+            <a className="scaleLinks" href="/" target="_blank">
               引きこもり家族会やまぼうし
             </a>
           </dd>
-
           <dd>
-            <a className="redLinks scaleLinks" href="/" target="_blank">
+            <a className="scaleLinks" href="/" target="_blank">
               UX女子会
             </a>
           </dd>
           <dd>
-            <a className="redLinks scaleLinks" href="/" target="_blank">
+            <a className="scaleLinks" href="/" target="_blank">
               ゆ〜るlinks
             </a>
           </dd>
