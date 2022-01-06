5423075a2c2cd0df3c780495632ae79724876a82
sou <opepelab@gmail.com>
2022-01-06 10:16:31 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,43 @@
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+
+  {
+    key: "Content-Security-Policy",
+    value: "self",
+  },
+];
+
 module.exports = {
-  reactStrictMode: true,
-}
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
