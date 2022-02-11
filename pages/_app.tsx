33c75efca15fd44c31397f738860575a8b2db5dc
sou <ocelot2828@gmail.com>
2022-02-11 18:40:24 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,6 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }): JSX.Elemen
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* <meta name="robots" content="noindex,nofollow" /> */}
       </Head>
       <Layout>
         <AnimatePresence exitBeforeEnter>
